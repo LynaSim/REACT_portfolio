48bc7f42c7917bba7cea4944b2a45e897cd2be30
LynaSim <lyna.cloud@gmail.com>
2026-04-01 02:12:59 +0100

    Working on photo gallery. Experimented with Bootstrap cards. Installed gsap. Implemented Masonry component from React Bits successfully.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import ProjectCard from './components/ProjectCard';
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { masonryItems } from './data/masonryData';
+import Masonry from './components/Masonry';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section>
-        <div>
-          <h1>Lyna Sim</h1>
-          <h1>Full Stack Web Developer</h1>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-    </>
-  )
+    return (
+        <>
+            <Container className="mt-5">
+                <Row>
+                    <Col>
+                        <div className="text-white text-center">
+                            <h1 className="h1-myname">Lyna Sim</h1>
+                            <h1>Full Stack Web Developer</h1>
+                        
+                        <button
+                            className="counter"
+                            onClick={() => setCount((count) => count + 1)}
+                        >
+                            Count is {count}
+                        </button>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="masonry-ctnr mt-5">
+                    <Col>
+                    <div style={{ minHeight: '600px', position: 'relative', marginBottom: '50px' }}>
+                        <Masonry
+                            items={masonryItems}
+                            ease="power3.out"
+                            duration={0.6}
+                            stagger={0.05}
+                            animateFrom="bottom"
+                            scaleOnHover
+                            hoverScale={0.95}
+                            blurToFocus
+                            colorShiftOnHover={false}
+                        />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12}>
+                    <h1 className="text-white text-center">My Work</h1>
+                    </Col>
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                </Row>
+            </Container>
+        </>
+    )
 }
 
 export default App
---
+++ src/data/masonryData.js
@@ -0,0 +1,52 @@
+export const masonryItems =
+[
+    {
+        id: "1",
+        img: "https://picsum.photos/id/1015/600/900?grayscale",
+        url: "",
+        height: 400,
+    },
+    {
+        id: "2",
+        img: "https://picsum.photos/id/1011/600/750?grayscale",
+        url: "",
+        height: 300,
+    },
+    {
+        id: "3",
+        img: "https://picsum.photos/id/1020/600/800?grayscale",
+        url: "",
+        height: 500,
+    },
+    {
+        id: "4",
+        img: "https://picsum.photos/id/1015/600/900?grayscale",
+        url: "",
+        height: 400,
+    },
+    {
+        id: "5",
+        img: "https://picsum.photos/id/1011/600/750?grayscale",
+        url: "",
+        height: 250,
+    },
+    {
+        id: "6",
+        img: "https://picsum.photos/id/1020/600/800?grayscale",
+        url: "",
+        height: 600,
+    },
+    {
+        id: "7",
+        img: "https://picsum.photos/id/1015/600/900?grayscale",
+        url: "",
+        height: 400,
+    },
+    {
+        id: "8",
+        img: "https://picsum.photos/id/1011/600/750?grayscale",
+        url: "",
+        height: 250,
+    }
+    // ... more items
+]

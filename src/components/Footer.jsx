6f72fe7c8c8d2e622f4cecdec41bd9b64b155b62
LynaSim <lyna.cloud@gmail.com>
2026-04-03 14:39:44 +0100

    Added a Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="border-top text-end bg-transparent text-white mt-5">
+            © 2026 React Portfolio. Made by Lyna Sim. All Rights Reserved.
+        </footer>
+    );
+}
+
+export default Footer;

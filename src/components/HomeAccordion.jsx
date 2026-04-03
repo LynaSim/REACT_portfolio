77d6d6dc65f96a435d5bde4a8b64469076812905
LynaSim <lyna.cloud@gmail.com>
2026-04-03 01:08:32 +0100

    Finished styling accordion for the most.

--- src/components/HomeAccordion.jsx
+++ src/components/HomeAccordion.jsx
@@ -6,8 +6,11 @@ import { useAccordionButton } from 'react-bootstrap/AccordionButton'
 import Card from 'react-bootstrap/Card';
 import './HomeAccordion.css';
 
-const PINK = 'rgba(255, 192, 203, 0.6)';
-const BLUE = 'rgba(0, 0, 255, 0.6)';
+// const PINK = 'rgba(255, 192, 203, 0.6)';
+// const BLUE = 'rgba(0, 0, 255, 0.6)';
+const UNDERLINE = 'underline';
+const NONE = 'none';
+
 
 function ContextAwareToggle({ children, eventKey, callback }) {
   const { activeEventKey } = useContext(AccordionContext);
@@ -22,8 +25,9 @@ function ContextAwareToggle({ children, eventKey, callback }) {
   return (
     <button
       type="button"
-      className="home-accordion-button border border-0 bg-transparent shrikhand-regular fs-3 text-white"
-      style={{ backgroundColor: isCurrentEventKey ? PINK : BLUE }}
+      className="border border-0 bg-transparent shrikhand-regular fs-3 text-white"
+      // style={{ backgroundColor: isCurrentEventKey ? PINK : BLUE }}
+      style={{ textDecoration: isCurrentEventKey ? UNDERLINE : NONE }}
       onClick={decoratedOnClick}
     >
       {children}
@@ -34,19 +38,27 @@ function ContextAwareToggle({ children, eventKey, callback }) {
 function HomeAccordion() {
   return (
     <Accordion defaultActiveKey="0">
-      <Card className="border border-0 bg-transparent">
-        <Card.Header className="border border-0 bg-transparent">
-          <ContextAwareToggle eventKey="0">Click me!</ContextAwareToggle>
+      <Card className="border border-0 bg-transparent text-white">
+        <Card.Header className="d-flex justify-content-center border border-0 bg-transparent">
+          <ContextAwareToggle eventKey="0">Overview</ContextAwareToggle>
         </Card.Header>
         <Accordion.Collapse eventKey="0">
           <Card.Body className="border border-0 bg-transparent">Hello! I'm Lyna and I've officially started my journey as a full stack web developer.</Card.Body>
         </Accordion.Collapse>
       </Card>
-      <Card className="border border-0 bg-transparent">
-        <Card.Header className="border border-0 bg-transparent">
-          <ContextAwareToggle eventKey="1">Click me!</ContextAwareToggle>
+      <Card className="border border-0 bg-transparent text-white">
+        <Card.Header className="d-flex justify-content-center border border-0 bg-transparent">
+          <ContextAwareToggle eventKey="1">Stack</ContextAwareToggle>
         </Card.Header>
         <Accordion.Collapse eventKey="1">
+          <Card.Body className="border border-0 bg-transparent">Hello! I am another body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card className="border border-0 bg-transparent text-white">
+        <Card.Header className="d-flex justify-content-center border border-0 bg-transparent">
+          <ContextAwareToggle eventKey="2">Projects</ContextAwareToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="2">
           <Card.Body className="border border-0 bg-transparent">Hello! I am another body</Card.Body>
         </Accordion.Collapse>
       </Card>

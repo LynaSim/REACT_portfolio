360bc805135be5c819d08540df666a69534f2869
LynaSim <lyna.cloud@gmail.com>
2026-04-03 14:42:25 +0100

    Installed icons from React Icons. Implemented icons as links. Styled icons.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,56 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
-import '../App.css';
+import { Link } from 'react-router-dom';
+import { FaGithubSquare, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import HomeAccordion from '../components/HomeAccordion';
+import Footer from '../components/Footer';
+import '../App.css';
 
 function Home() {
     const [count, setCount] = useState(0);
 
-    return(
-<Container className="mt-5">
-                    <Row>
-                        <Col>
-                            <header className="text-white text-center">
-                                <h1 className="h1-myname">Lyna Sim</h1>
-                                <h1>Full Stack Web Developer</h1>
-                                <button
-                                    className="counter"
-                                    onClick={() => setCount((count) => count + 1)}>
-                                    Count is {count}
-                                </button>
-                            </header>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <HomeAccordion />
-                        </Col>
-                    </Row>
+    return (
+        <Container className="mt-5">
+            <Row>
+                <Col>
+                    <header className="text-white text-center">
+                        <h1 className="h1-myname">Lyna Sim</h1>
+                        <h1>Full Stack Web Developer</h1>
+                        <button
+                            className="counter"
+                            onClick={() => setCount((count) => count + 1)}>
+                            Count is {count}
+                        </button>
+                    </header>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <HomeAccordion />
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                <Link to="https://github.com/LynaSim" aria-label="GitHub">
+                    <FaGithubSquare style={{ fontSize: '3rem', color: 'white' }} />
+                </Link>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                    <FaLinkedin style={{ fontSize: '3rem', color: 'white' }} />
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                    <FaEnvelope style={{ fontSize: '2.5rem', color: 'white' }} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Footer />
+                </Col>
+            </Row>
         </Container>
     );
 }

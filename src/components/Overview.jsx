554a09351b6e20a338c4d95343619a789506bf80
LynaSim <lyna.cloud@gmail.com>
2026-05-16 16:15:48 +0100

    Bit of cleanup and a bit refactoring.

---
+++ src/components/Overview.jsx
@@ -0,0 +1,14 @@
+import Card from 'react-bootstrap/Card';
+
+
+function Overview() {
+    return (
+        <Card.Body className="border border-0 bg-transparent text-center">Hello! I'm Lyna, and I've just concluded a 12-week web development bootcamp, officially starting my path as a full-stack developer!<br /><br />
+            My journey began in the publishing industry as an XML content specialist, where I prepared complex XML data structures for the next step of their lifecycle. While collaborating with the development team, I became deeply fascinated with how raw data was transformed into interactive user experiences.<br />
+            Years later, after time dedicated to my family and a very rewarding chapter in SEN education, I have returned to my technical roots by completing this intensive but thoroughly enjoyable 12-week full-stack bootcamp.<br /><br />
+            I cannot wait for opportunities to combine my diverse skills and experiences into building applications I feel passionate about!
+        </Card.Body>
+    );
+}
+
+export default Overview;
